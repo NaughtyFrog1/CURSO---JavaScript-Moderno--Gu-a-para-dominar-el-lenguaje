2260c123ff21e15c540e4e8f32de5d4fc445a109
Manuel Silenzi <manuelsilenzi1@gmail.com>
2023-03-23 14:58:00 -0300

    improve button styles

--- 08-CRUDApp/src/components/NewUserButton/NewUserButton.js
+++ 08-CRUDApp/src/components/NewUserButton/NewUserButton.js
@@ -1,15 +1,16 @@
+import ModalNewUser from '../Modal/ModalNewUser'
 import './NewUserButton.css'
 
 function NewUserButton(parentNode) {
   const $newUserBtn = document.createElement('button')
-  $newUserBtn.classList.add('btn', 'new-user-btn', 'bg-light')
+  $newUserBtn.classList.add('btn', 'btn--primary', 'bg-light', 'new-user-btn')
   $newUserBtn.innerHTML = '&plus;'
 
   parentNode.append($newUserBtn)
 
-  $newUserBtn.addEventListener('click', () => {
-    console.log('click new user')
-  })
+  const modal = ModalNewUser()
+
+  $newUserBtn.addEventListener('click', modal.showModal)
 }
 
 export default NewUserButton
